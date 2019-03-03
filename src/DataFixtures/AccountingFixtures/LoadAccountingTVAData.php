2ea786ad4b95f215dbf96eb10d0ce2b22b5d2d9e
jbjog <jeanbaptiste.jogeix@outlook.fr>
2019-03-03 03:49:36 +0100

    feat(accounting): add accounting relations and fixtures

---
+++ src/DataFixtures/AccountingFixtures/LoadAccountingTVAData.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures\AccountingFixtures;
+
+use App\DataFixtures\MaderaFixtures;
+use App\Entity\Accounting\AccountingTVA;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadAccountingTVAData extends MaderaFixtures{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager) {
+        $tva196 = new AccountingTVA();
+        $tva196->setLabel("19,6%")
+            ->setValue(0.196)
+            ->setApplicable(false);
+        $manager ->persist($tva196);
+
+        $tva20 = new AccountingTVA();
+        $tva20->setLabel("20%")
+            ->setValue(0.2)
+            ->setApplicable(true);
+        $manager ->persist($tva196);
+
+        $manager->flush();
+
+        $this->addReference('accounting-tva-196', $tva196);
+        $this->addReference('accounting-tva-20', $tva20);
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder() {
+        return MaderaFixtures::ACCOUNTING_TVA;
+    }
+}
---
+++ src/DataFixtures/AccountingFixtures/LoadAccountingValueRateData.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: JB
+ * Date: 03/03/2019
+ * Time: 03:39
+ */
+
+namespace App\DataFixtures\AccountingFixtures;
+
+
+class LoadAccountingValueRateData {
+
+}
