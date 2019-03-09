294adfaed265c8acdbcad2f04d3561862c8cc2e4
jbjog <jeanbaptiste.jogeix@outlook.fr>
2019-03-09 15:21:01 +0100

    feat(Range) : add range relations and fictures

--- src/DataFixtures/RangeFixtures/LoadRangeData.php
+++ src/DataFixtures/RangeFixtures/LoadRangeData.php
@@ -1,14 +1,51 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: JB
- * Date: 06/03/2019
- * Time: 20:15
- */
 
 namespace App\DataFixtures\RangeFixtures;
 
+use App\DataFixtures\ComponentFixtures\LoadComponentCoveringData;
+use App\DataFixtures\ComponentFixtures\LoadComponentExteriorFinishData;
+use App\DataFixtures\ComponentFixtures\LoadComponentInsulationData;
+use App\DataFixtures\ComponentFixtures\LoadComponentInteriorFinishData;
+use App\DataFixtures\ComponentFixtures\LoadComponentWindowFrameData;
+use App\DataFixtures\MaderaFixtures;
+use App\Entity\Range\Range;
+use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadRangeData {
+class LoadRangeData extends MaderaFixtures{
 
+    const PREMIUM = 'range-range-premium';
+    const CLASSIC = 'range-range-classic';
+    public function load(ObjectManager $manager) {
+        $premium = new Range();
+        $premium->setLabel("Premium")
+                ->setComponentCovering($this->getReference(LoadComponentCoveringData::TUILES))
+                ->setComponentExteriorFinish($this->getReference(LoadComponentExteriorFinishData::CREPIS))
+                ->setComponentInsulation($this->getReference(LoadComponentInsulationData::SYNTH))
+                ->setComponentInteriorFinish($this->getReference(LoadComponentInteriorFinishData::GYPSE))
+                ->setComponentWindowFrame($this->getReference(LoadComponentWindowFrameData::DOUBLE));
+        $manager ->persist($premium);
+
+        $classic = new Range();
+        $classic->setLabel("Classique")
+                ->setComponentCovering($this->getReference(LoadComponentCoveringData::ARDOISES))
+                ->setComponentExteriorFinish($this->getReference(LoadComponentExteriorFinishData::BOIS))
+                ->setComponentInsulation($this->getReference(LoadComponentInsulationData::NATUREL))
+                ->setComponentInteriorFinish($this->getReference(LoadComponentInteriorFinishData::PLATRE))
+                ->setComponentWindowFrame($this->getReference(LoadComponentWindowFrameData::CLASSIC));
+        $manager ->persist($classic);
+
+        $manager->flush();
+
+        $this->addReference(LoadRangeData::PREMIUM, $premium);
+        $this->addReference(LoadRangeData::CLASSIC, $classic);
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder() {
+        return MaderaFixtures::ACCOUNTING_MARGIN;
+    }
 }
--- src/Entity/Range/Range.php
+++ src/Entity/Range/Range.php
@@ -1,30 +1,76 @@
 <?php
-/**
- * Created by Jules Aubel
- * Date: 15/02/19
- */
 
-namespace App\Entity\Margin;
+namespace App\Entity\Range;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Entity\Component\ComponentCovering;
+use App\Entity\Component\ComponentExteriorFinish;
+use App\Entity\Component\ComponentInsulation;
+use App\Entity\Component\ComponentInteriorFinish;
+use App\Entity\Component\ComponentWindowFrame;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Class Margin
- * @package App\Entity\Margin
+ * Class Range
+ * @package App\Entity\Range
  * @ORM\Entity
- * @ORM\Table(name="MARGIN_margin")
+ * @ORM\Table(name="RANGE_range")
  * @ApiResource()
  */
-class Margin
+class Range
 {
+
+    //////////////////////////////////
+    // RELATIONS
+    //////////////////////////////////
+
+    /**
+     * @var ComponentCovering $componentCovering
+     *
+     * @ORM\ManyToOne(targetEntity="\App\Entity\Component\ComponentCovering")
+     * @ORM\JoinColumn(name="component_covering_id", referencedColumnName="id", onDelete="RESTRICT")
+     */
+    private $componentCovering;
+
+    /**
+     * @var ComponentExteriorFinish $componentExteriorFinish
+     *
+     * @ORM\ManyToOne(targetEntity="\App\Entity\Component\ComponentExteriorFinish")
+     * @ORM\JoinColumn(name="component_exterior_finish_id", referencedColumnName="id", onDelete="RESTRICT")
+     */
+    private $componentExteriorFinish;
+
+
+    /**
+     * @var ComponentInteriorFinish $componentInteriorFinish
+     *
+     * @ORM\ManyToOne(targetEntity="\App\Entity\Component\ComponentInteriorFinish")
+     * @ORM\JoinColumn(name="component_interior_finish_id", referencedColumnName="id", onDelete="RESTRICT")
+     */
+    private $componentInteriorFinish;
+
+    /**
+     * @var ComponentInsulation $componentInsulation
+     *
+     * @ORM\ManyToOne(targetEntity="\App\Entity\Component\ComponentInsulation")
+     * @ORM\JoinColumn(name="component_insulation_id", referencedColumnName="id", onDelete="RESTRICT")
+     */
+    private $componentInsulation;
+
+    /**
+     * @var ComponentWindowFrame $componentWindowFrame
+     *
+     * @ORM\ManyToOne(targetEntity="\App\Entity\Component\ComponentWindowFrame")
+     * @ORM\JoinColumn(name="component_window_frame_id", referencedColumnName="id", onDelete="RESTRICT")
+     */
+    private $componentWindowFrame;
 
     //////////////////////////////////
     // PROPERTIES
     //////////////////////////////////
 
     /**
-     * @var int Margin Id
+     * @var int Range Id
      *
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -33,7 +79,7 @@ class Margin
     private $id;
 
     /**
-     * @var string Margin Label
+     * @var string Range Label
      *
      * @ORM\Column(type="string", nullable=false)
      */
@@ -49,9 +95,9 @@ class Margin
 
     /**
      * @param int $id
-     * @return Margin
+     * @return Range
      */
-    public function setId(int $id): Margin
+    public function setId(int $id): Range
     {
         $this->id = $id;
         return $this;
@@ -67,11 +113,91 @@ class Margin
 
     /**
      * @param string $label
-     * @return Margin
+     * @return Range
      */
-    public function setLabel(string $label): Margin
+    public function setLabel(string $label): Range
     {
         $this->label = $label;
+        return $this;
+    }
+
+    /**
+     * @return ComponentCovering
+     */
+    public function getComponentCovering(): ComponentCovering {
+        return $this->componentCovering;
+    }
+
+    /**
+     * @param ComponentCovering $componentCovering
+     * @return Range
+     */
+    public function setComponentCovering(ComponentCovering $componentCovering): Range {
+        $this->componentCovering = $componentCovering;
+        return $this;
+    }
+
+    /**
+     * @return ComponentExteriorFinish
+     */
+    public function getComponentExteriorFinish(): ComponentExteriorFinish {
+        return $this->componentExteriorFinish;
+    }
+
+    /**
+     * @param ComponentExteriorFinish $componentExteriorFinish
+     * @return Range
+     */
+    public function setComponentExteriorFinish(ComponentExteriorFinish $componentExteriorFinish): Range{
+        $this->componentExteriorFinish = $componentExteriorFinish;
+        return $this;
+    }
+
+    /**
+     * @return ComponentInteriorFinish
+     */
+    public function getComponentInteriorFinish(): ComponentInteriorFinish {
+        return $this->componentInteriorFinish;
+    }
+
+    /**
+     * @param ComponentInteriorFinish $componentInteriorFinish
+     * @return Range
+     */
+    public function setComponentInteriorFinish(ComponentInteriorFinish $componentInteriorFinish): Range{
+        $this->componentInteriorFinish = $componentInteriorFinish;
+        return $this;
+    }
+
+    /**
+     * @return ComponentInsulation
+     */
+    public function getComponentInsulation(): ComponentInsulation {
+        return $this->componentInsulation;
+    }
+
+    /**
+     * @param ComponentInsulation $componentInsulation
+     * @return Range
+     */
+    public function setComponentInsulation(ComponentInsulation $componentInsulation): Range{
+        $this->componentInsulation = $componentInsulation;
+        return $this;
+    }
+
+    /**
+     * @return ComponentWindowFrame
+     */
+    public function getComponentWindowFrame(): ComponentWindowFrame {
+        return $this->componentWindowFrame;
+    }
+
+    /**
+     * @param ComponentWindowFrame $componentWindowFrame
+     * @return Range
+     */
+    public function setComponentWindowFrame(ComponentWindowFrame $componentWindowFrame): Range {
+        $this->componentWindowFrame = $componentWindowFrame;
         return $this;
     }
 
