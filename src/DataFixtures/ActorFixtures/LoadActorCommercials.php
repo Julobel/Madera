c7cc39e907db66bf632d11934f6ec71f50533a91
Jules Aubel <jules.aubel@protonmail.com>
2019-03-09 21:46:08 +0100

    feat(actor): add fixtures

---
+++ src/DataFixtures/ActorFixtures/LoadActorCommercials.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by Jules Aubel
+ * Date: 09/03/19
+ */
+
+namespace App\DataFixtures\ActorFixtures;
+
+
+use App\DataFixtures\MaderaFixtures;
+use App\Entity\Actor\ActorCommercial;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadActorCommercials extends MaderaFixtures
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $commercial = new ActorCommercial();
+            $commercial->setFirstName('Prénom' . $i);
+            $commercial->setLastName('Nom' . $i);
+            $commercial->setEmail('clientMail' . $i . '@mail.fr');
+            $commercial->setPhoneNUmber('060606060' . $i);
+
+            $manager->persist($commercial);
+        }
+        $manager->flush();
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return MaderaFixtures::ACTOR_COMMERCIAL;
+    }
+}
